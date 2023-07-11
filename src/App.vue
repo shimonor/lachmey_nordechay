8ef8c743574571c9b2a0aead03e71f3d7283bf60
shimonor <S0548493476@gmail.com>
2023-07-12 00:27:10 +0300

    aaaaaa

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <topPage />
+</template>
+
+<script setup>
+import topPage from './components/topPage.vue'
+</script>
+
+<style>
+body{
+  background-color: rgba(148, 96, 1, 0.541);
+  background-image: url(../public/logo.jpg);
+  background-size:100vw 100vh ;
+}
+</style>
---
+++ src/components/topPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>לחמי מרדכי</h2>
+
+  </div>
+</template>
+
+<style scoped lang="scss">
+$colorText: blue;
+
+
+div h2 {
+  color: $colorText;
+}
+</style>
